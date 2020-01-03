bbeb22e477dfd0d3b11046454769a049a5db28dc
RHAYMISON <rhaymison@hotmail.com>
2020-01-03 20:58:46 -0300

    initial commit

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  @ViewChild(IonSlides, { static: false }) slides: IonSlides;
+  public wavesPosition: number = 0;
+  public wavesDifference: number = 50;
+
+  constructor(public keyboard: Keyboard ) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+
+  }
+
+  register() {
+
+  }
+
+  segmentChanged(event: any) {
+    if (event.detail.value === 'login') {
+      this.slides.slidePrev();
+      this.wavesPosition += this.wavesDifference;
+    } else {
+      this.slides.slideNext();
+      this.wavesPosition -= this.wavesDifference;
+    }
+  }
+
+}
